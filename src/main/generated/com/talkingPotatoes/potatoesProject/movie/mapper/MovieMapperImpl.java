20afc25c878fdf3f2bab970c99dd52ec4057b2d8
n0nim424 <n0nim424@gmail.com>
2023-06-16 17:37:39 +0900

    [#3] 데이터 여러개인 항목 파싱해서 저장

--- src/main/generated/com/talkingPotatoes/potatoesProject/movie/mapper/MovieMapperImpl.java
+++ src/main/generated/com/talkingPotatoes/potatoesProject/movie/mapper/MovieMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-15T23:49:29+0900",
+    date = "2023-06-16T17:15:00+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
--- src/main/java/com/talkingPotatoes/potatoesProject/movie/scheduler/MovieApiScheduler.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/movie/scheduler/MovieApiScheduler.java
@@ -16,7 +16,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -25,7 +27,7 @@ public class MovieApiScheduler {
 
     private final IMovieService movieService;
 
-    @Scheduled(cron = "10 * * * * *")
+    @Scheduled(cron = "0/10 * * * * *")
     public void getData() throws Exception{
         /* open api 샘플코드 */
         /* StringBuilder에 json 데이터 line으로 읽어서 저장 */
@@ -73,21 +75,82 @@ public class MovieApiScheduler {
         for (int i = 0; i < result.size(); i++) {
             JSONObject movieObj = (JSONObject) result.get(i);
 
+            /* 감독 배열 파싱해서 String으로 저장 */
+            String directorNm = "";
+            String directorEnNm = "";
+            String directorId = "";
+
+            JSONObject directors = (JSONObject) movieObj.get("directors");
+            JSONArray directorArray = (JSONArray) directors.get("director");
+            if(directorArray!=null && directorArray.size()>0) {
+                JSONObject director = (JSONObject) directorArray.get(0);
+                directorNm = (String) director.get("directorNm");
+                directorEnNm = (String) director.get("directorEnNm");
+                directorId = (String) director.get("directorId");
+
+                for(int j=1; j<directorArray.size(); j++) {
+                    director = (JSONObject) directorArray.get(j);
+                    directorNm += ", " + director.get("directorNm");
+                    directorEnNm += ", " + director.get("directorEnNm");
+                    directorId += ", " + director.get("directorId");
+                }
+            }
+
+            /* 배우 배열 파싱해서 String으로 저장 */
+            String actorNm = "";
+            String actorEnNm = "";
+            String actorId = "";
+
+            JSONObject actors = (JSONObject) movieObj.get("actors");
+            JSONArray actorArray = (JSONArray) actors.get("actor");
+            if(actorArray!=null && actorArray.size()>0) {
+                JSONObject actor = (JSONObject) actorArray.get(0);
+                actorNm = (String) actor.get("actorNm");
+                actorEnNm = (String) actor.get("actorEnNm");
+                actorId = (String) actor.get("actorId");
+
+                for(int j=1; j<actorArray.size(); j++) {
+                    actor = (JSONObject) actorArray.get(j);
+                    actorNm += ", " + actor.get("actorNm");
+                    actorEnNm += ", " + actor.get("actorEnNm");
+                    actorId += ", " + actor.get("actorId");
+                }
+            }
+
+            /* 줄거리 배열 파싱해서 한국어 줄거리 저장, 한국어 없으면 영어 */
+            Map<String, String> plotMap = new HashMap<>();
+            String plot = "";
+
+            JSONObject plots = (JSONObject) movieObj.get("plots");
+            JSONArray plotArray = (JSONArray) plots.get("plot");
+            for(int j=0; j<plotArray.size(); j++) {
+                JSONObject plotData = (JSONObject) plotArray.get(j);
+                plotMap.put((String) plotData.get("plotLang"), (String) plotData.get("plotText"));
+            }
+
+            if(plotMap.containsKey("한국어")) {
+                plot = plotMap.get("한국어");
+            }
+            else if(plotMap.containsKey("영어")) {
+                plot = plotMap.get("영어");
+            }
+
+            /* dto에 데이터 저장 */
             MovieDto movieDto = MovieDto.builder()
                     .docId((String) movieObj.get("DOCID"))
                     .title((String) movieObj.get("title"))
                     .titleEng((String) movieObj.get("titleEng"))
                     .titleOrg((String) movieObj.get("titleOrg"))
-                    //.directorNm((String) movieObj.get("DOCID"))
-                    //.directorEnNm((String) movieObj.get("DOCID"))
-                    //.directorId((String) movieObj.get("DOCID"))
-                    //.actorNm((String) movieObj.get("DOCID"))
-                    //.actorEnNm((String) movieObj.get("DOCID"))
-                    //.actorId((String) movieObj.get("DOCID"))
+                    .directorNm(directorNm)
+                    .directorEnNm(directorEnNm)
+                    .directorId(directorId)
+                    .actorNm(actorNm)
+                    .actorEnNm(actorEnNm)
+                    .actorId(actorId)
                     .nation((String) movieObj.get("nation"))
                     .company((String) movieObj.get("company"))
                     .prodYear((String) movieObj.get("prodYear"))
-                    //.plot((String) movieObj.get("DOCID"))
+                    .plot(plot)
                     .runtime((String) movieObj.get("runtime"))
                     .rating((String) movieObj.get("rating"))
                     .genre((String) movieObj.get("genre"))
@@ -97,6 +160,7 @@ public class MovieApiScheduler {
                     .stillUrl((String) movieObj.get("stlls"))
                     .build();
 
+            /* list에 dto 담기 */
             list.add(movieDto);
         }
 
