2209ff5837ce30f27722e769d8a6677dc1d9caf6
hyewon11 <hwj1104@naver.com>
2023-08-14 01:12:22 +0900

    [#37] feat: 일별 박스오피스 조회 API

--- src/main/java/com/talkingPotatoes/potatoesProject/movie/repository/BoxOfficeRateRepository.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/movie/repository/BoxOfficeRateRepository.java
@@ -2,6 +2,7 @@ package com.talkingPotatoes.potatoesProject.movie.repository;
 
 import com.talkingPotatoes.potatoesProject.movie.entity.BoxOfficeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,5 +11,5 @@ import java.util.List;
 
 @Repository
 public interface BoxOfficeRateRepository extends JpaRepository<BoxOfficeRate, Long> {
-    List<BoxOfficeRate> findByCreatedAt(LocalDate dateTime);
+    List<BoxOfficeRate> findAllByTargetDt(String targetDt);
 }
