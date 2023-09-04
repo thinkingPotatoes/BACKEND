58839d8b69323c89eb0894da2c09265a7f323769
yejin013 <yejin013@naver.com>
2023-09-04 19:02:13 +0900

    fix: 초 수정

--- src/main/java/com/talkingPotatoes/potatoesProject/common/util/EmailUtil.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/common/util/EmailUtil.java
@@ -41,7 +41,7 @@ public class EmailUtil {
     public String createToken(UserDto dto) {
         UUID random = UUID.randomUUID();
 
-        redisUtil.setDataExpire(String.valueOf(random), String.valueOf(dto.getId()), 30);
+        redisUtil.setDataExpire(String.valueOf(random), String.valueOf(dto.getId()), 1800);
 
         return String.valueOf(random);
     }
