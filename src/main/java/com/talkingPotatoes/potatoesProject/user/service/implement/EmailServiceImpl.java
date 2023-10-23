eb7699707ff9bb11937d94e562e918c1e28f9ddd
채예진 <45252618+yejin013@users.noreply.github.com>
2023-10-23 21:36:02 +0900

    [#1] 이메일 redirect uri 수정

--- src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/EmailServiceImpl.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/EmailServiceImpl.java
@@ -36,7 +36,7 @@ public class EmailServiceImpl implements EmailService {
                 + "<h1> 안녕하세요. Filmo 입니다</h1>"
                 + "<br>"
                 + "<p>아래 링크를 클릭하면 이메일 인증이 완료됩니다.<p>"
-                + "<a href='localhost:5173/register/success?token=" + emailUtil.createToken(to) + "'>인증 링크</a>"
+                + "<a href='http://localhost:5173/register/success?token=" + emailUtil.createToken(to) + "'>인증 링크</a>"
                 + "</div>";
 
         emailUtil.createMessage(to.getUserId(), message);
