17c2d73c7403707b1cecb783330b220a31901d8a
yejin013 <yejin013@naver.com>
2023-11-05 13:41:44 +0900

    schedule 수정

--- src/main/java/com/talkingPotatoes/potatoesProject/movie/scheduler/MovieParsingScheduler.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/movie/scheduler/MovieParsingScheduler.java
@@ -37,7 +37,7 @@ public class MovieParsingScheduler {
     private final StaffMapper staffMapper;
     private final StillMapper stillMapper;
 
-    @Scheduled(cron = "0 5 1 ? * MON")  // API 배치 돌리고 5분 뒤
+    @Scheduled(cron = "0 55 13 ? * MON")  // API 배치 돌리고 5분 뒤
     public void getData() throws Exception{
         /* 실행 시간 재는 코드 */
         StopWatch stopWatch = new StopWatch();
