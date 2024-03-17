4da190c79028d65aa581a465db687d850c22d322
n0nim424 <n0nim424@gmail.com>
2024-03-17 17:41:51 +0900

    IPv4 주소 -> IPv4 DNS

--- src/main/java/com/talkingPotatoes/potatoesProject/common/config/WebConfig.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5100", "http://127.0.0.1:5100", "http://3.38.67.125:5100")
+                .allowedOrigins("http://localhost:5100", "http://127.0.0.1:5100", "http://3.38.67.125:5100", "http://ec2-3-38-67-125.ap-northeast-2.compute.amazonaws.com:5100")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true)
                 .exposedHeaders("*");
