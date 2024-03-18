8d0f122bff6caafefbfaae1eb58f0265838ea31a
채예진 <45252618+yejin013@users.noreply.github.com>
2024-03-18 21:57:04 +0900

    ACTIVE 강제

--- src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/UserServiceImpl.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/UserServiceImpl.java
@@ -55,7 +55,7 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsUserByUserIdAndRole(userDto.getUserId(), Role.ACTIVE))
             throw new DuplicationException("이메일 중복입니다.");
 
-        if (userDto.getRole() == null) userDto.setRole(Role.ACTIVE);
+        userDto.setRole(Role.ACTIVE);
 
         String nickname = "F_"+ randomNickname.makeNickname();
         while(userRepository.existsByNickname(nickname)) {
