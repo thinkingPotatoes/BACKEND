0ec7cb11d8347a660af2655191ae7e831c74ad11
강유진 <45729009+n0nim424@users.noreply.github.com>
2024-03-18 23:01:54 +0900

    Update UserServiceImpl.java

--- src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/UserServiceImpl.java
+++ src/main/java/com/talkingPotatoes/potatoesProject/user/service/implement/UserServiceImpl.java
@@ -55,7 +55,9 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsUserByUserIdAndRole(userDto.getUserId(), Role.ACTIVE))
             throw new DuplicationException("이메일 중복입니다.");
 
+        log.debug(userDto.getRole());
         userDto.setRole(Role.ACTIVE);
+        log.debug(userDto.getRole());
 
         String nickname = "F_"+ randomNickname.makeNickname();
         while(userRepository.existsByNickname(nickname)) {
